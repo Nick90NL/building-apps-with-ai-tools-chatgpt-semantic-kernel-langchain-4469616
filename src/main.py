0129886cb98eddf5343cb644bee84b2aaba01996
Nick90NL <95159828+Nick90NL@users.noreply.github.com>
2023-12-30 00:43:12 +0100

    yes

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
+    model="gpt-4-1106-preview",
     messages=[
         {"role": "system", "content": ""},
         {"role": "user", "content": "Say 'Hello world'"}
