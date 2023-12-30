78a43855f46075c248593a9c0a69617e17b5b8e8
Nick90NL <95159828+Nick90NL@users.noreply.github.com>
2023-12-30 01:39:19 +0100

    3

--- src/services/ai.py
+++ src/services/ai.py
@@ -3,7 +3,6 @@ import openai
 from dotenv import load_dotenv
 load_dotenv()
 
-
 def generate_review(review):
     openai.api_key = os.getenv('OPENAI_API_KEY')
     response = openai.ChatCompletion.create(
@@ -18,7 +17,5 @@ def generate_review(review):
 
     response_message = response["choices"][0]["message"]["content"]
     if response_message == "happy":
-        # TODO 1
-        return ""
-    # TODO 2
-    return ""
+        return "Thanks for shopping with us, come back soon!"
+    return "Sorry to hear about your experience, here is a coupon for 20% off, typ GPT20 to use it."
